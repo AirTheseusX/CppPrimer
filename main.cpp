628b47a2d577bc6de07042b487a95384530c4eab
Airtheseus <airtheseusgungun@gmail.com>
2024-07-24 23:34:58 +0800

    cpp primer test.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void foo(int &x)
+{
+    cout << "x: " << x << endl;
+    x = 11;
+}
+int main()
+{
+    int a = 10;
+    int &b = a;
+
+    foo(a);
+    foo(b);
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+}
