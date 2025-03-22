1581a34a60eb2cd10390204f6797d8653350de9e
AirTheseus <airtheseusgungun@outlook.com>
2025-03-22 17:30:23 +0800

    Update person.cpp

--- source/person.cpp
+++ source/person.cpp
@@ -1,5 +1,12 @@
 #include "person.h"
 
+using std::istream;
+using std::ostream;
+
+Person::Person(istream &is) {
+    readPerson(is);
+}
+
 string Person::getName() const {
     return this->name;
 }
@@ -8,3 +15,11 @@ string Person::getAddress() const {
     return this->address;
 }
 
+void Person::readPerson(istream &is) {
+    is >> this->name >> this->address;
+}
+
+void Person::printPerson(ostream &os) {
+    os << "name: " << this->name << std::endl;
+    os << "address: " << this->address << std::endl;
+}
