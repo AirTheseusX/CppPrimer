2da028a2a546b1bc2311c00ac7c4a2b19d71430f
AirTheseusX <airtheseusgungun@outlook.com>
2025-03-22 19:21:21 +0800

    cpp primer test.

--- main.cpp
+++ main.cpp
@@ -17,12 +17,6 @@ using std::initializer_list;
 
 int main(int argc, char *arg[])
 {
-    /*
-    Sales_data() = default;
-    Sales_data(const string s) : bookNo(s) {}
-    Sales_data(const string s, unsigned int sold, double revenue) : bookNo(s), units_sold(sold), revenue(revenue) {}
-    Sales_data(istream &is);
-    */
     Sales_data sales1;
     Sales_data sales2("Happy Day!");
     Sales_data sales3("Hello!", 10, 9.99);
